db16132a026713bdd3e637e4c6ae5d3e1f1935c3
K-Sato <katsuki_sato0130@icloud.com>
2022-04-12 16:43:23 +0900

    PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /next.config.js
+/** @type {import('next/dist/next-server/server/config-shared').NextConfig} */
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const config = {
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+};
+
+module.exports = withPWA(config);
